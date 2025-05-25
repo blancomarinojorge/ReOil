ff534adca23c809ab925fce90781be423b4dcc93
Jorge Blanco Mariño <blancomarinojorge@gmail.com>
2025-05-25 19:59:08 +0200

    Entorno listo para traballar

---
+++ app/laravel/application/tests/Feature/ExampleTest.php
@@ -0,0 +1,7 @@
+<?php
+
+it('adds two numbers', function () {
+    $sum = 2 + 3;
+
+    expect($sum)->toBe(5);
+});
---
+++ app/laravel/application/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        watch: {
+            usePolling: true,
+        },
+        hmr: {
+            host: 'localhost',
+            protocol: 'ws',
+            clientPort: 5173,
+        }
+    },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+});
