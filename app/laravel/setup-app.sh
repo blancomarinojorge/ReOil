b02745f48d273b9543cc2a1fb5c35d21fd7bbd0d
Jorge Blanco Mariño <blancomarinojorge@gmail.com>
2025-06-18 12:26:31 +0200

    Script de creación de entorno actualizado

--- app/laravel/setup-app.sh
+++ app/laravel/setup-app.sh
@@ -9,3 +9,30 @@ sudo chown -R "$(id -u)":"$(id -g)" .
 
 # 3. docker compose e build
 docker compose up -d --build
+
+# 4. creacion das tablsa da base de datos
+## espero a que mysql estea listo para recibir conexions, se tarda mais de 30 segundos canceloo
+echo "Esperando a que mysql estea listo para facer as migracions..."
+
+MAX_RETRIES=15 #15*2seg = 30seg
+count=0
+until docker exec laravel_mysql mysqladmin ping -h "localhost" --silent; do
+  count=$((count+1))
+  if [ $count -ge $MAX_RETRIES ]; then
+    echo "Non se puido establecer conexions co contedor mysql, cancelando migracions..."
+    exit 1
+  fi
+
+  echo "A base de datos sigue sen estar lista... esperando 2 segundos"
+  sleep 2
+done
+
+echo "Base de datos lista, facendo migracions..."
+sleep 4
+docker exec -it laravel_app php artisan migrate || {
+  echo "Erro executando as migracions, podes intentalo de novo co comando:"
+  echo "docker exec -it laravel_app php artisan migrate"
+  exit 1
+}
+
+echo "\nTodo listo! Podes acceder a aplicación en http://localhost:8000"
